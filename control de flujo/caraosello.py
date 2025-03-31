24f917ad976c42db8b830fdaa2268e454a6ce340
wkl0ree <162451285+wkl0ree@users.noreply.github.com>
2025-03-31 11:20:20 -0300

    Add files via upload

---
+++ control de flujo/caraosello.py
@@ -0,0 +1,12 @@
+#lreto vargas
+#31-03-2025
+#volvi a tener robux
+
+import random
+
+num = random.randint(0,1)
+
+if num > 0.5 :
+    print('cara')
+else:
+    print('sello')
---
+++ control de flujo/promedio.py
@@ -0,0 +1,10 @@
+#lreto vargas
+#31-03-2025
+#volvi a tener robux
+
+nota = 70
+
+if nota > 0.70 :
+    print('tu pasaste.')
+else:
+    print('tu repruebas.')
